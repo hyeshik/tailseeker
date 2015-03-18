ae6ad2717cd9a043c2f1460103e8c61cf85551a7
Hyeshik Chang <hyeshik@snu.ac.kr>
2015-03-18 21:56:23 +0900

    Port `tailseqext' python module to Python 3 (only).

--- tailor/setup.py
+++ tailor/setup.py
@@ -1,18 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+#
+# Copyright (c) 2015 Hyeshik Chang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 import sys
 from distutils.core import setup, Extension
 
-TAILSEQ_MEGADEPS = '/home/tsuser/ts-deps'
-NUMPY_INCLUDE = '{prefix}/lib/{ver}/site-packages/numpy/core/include'.format(
-                       prefix=TAILSEQ_MEGADEPS, ver='python{}.{}'.format(*sys.version_info[:2]))
-
-setup (name = 'tailseqext',
-       version = '0.1',
-       description = 'extension modules for tail-seq',
-       author = 'Hyeshik Chang',
-       author_email = 'hyeshik@snu.ac.kr',
-       url = 'http://www.narrykim.org',
-       license = 'MIT',
-       ext_modules = [Extension('tailseqext', ['tailseqext.c'])],
-       include_dirs = [NUMPY_INCLUDE]
+setup(name = 'tailseqext',
+      version = '0.2',
+      description = 'extension modules for TAIL-seq',
+      author = 'Hyeshik Chang',
+      author_email = 'hyeshik@snu.ac.kr',
+      url = 'http://highthroughput.org',
+      license = 'MIT',
+      ext_modules = [Extension('tailseqext', ['src/pythonext/tailseqext.c'])]
 )
--- tailor/src/pythonext/tailseqext.c
+++ tailor/src/pythonext/tailseqext.c
@@ -1,43 +1,39 @@
 /**
- * Assorted accelerator functions for Tail-seq from Narry Kim Lab.
+ * Assorted accelerator functions for TAIL-seq from Narry Kim Lab.
+ *
+ * Copyright (c) 2014-5 Institute of Basic Sciences
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  * Hyeshik Chang <hyeshik@snu.ac.kr>
- *
  */
 
 #define PY_SSIZE_T_CLEAN
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
 #include "Python.h"
 #include <numpy/arrayobject.h>
-
-static PyObject *ErrorObject;
 
 #define NCHANNELS   4
 
 #define SIGNAL_MIN  -255
 #define CODED_MAX   4095
-
-static const char *base64_enc_table="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
-                                    "ghijklmnopqrstuvwxyz0123456789+/";
-
-#define NE          64
-static const int16_t base64_dec_table[256]={
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, 62, NE, NE, NE, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, NE, NE, NE, NE, NE, NE,
-    NE,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, NE, NE, NE, NE, NE,
-    NE, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE,
-    NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE, NE
-};
 
 
 static int
@@ -49,10 +45,10 @@ decode_intensity(const char *encoded, int length, int16_t *signals)
         for (chan = 0; chan < NCHANNELS; chan++) {
             int16_t high, low;
 
-            high = base64_dec_table[(unsigned char)*(encoded++)];
-            low = base64_dec_table[(unsigned char)*(encoded++)];
+            high = (int16_t)*(encoded++) - 33;
+            low = (int16_t)*(encoded++) - 33;
 
-            if (high == NE || low == NE) {
+            if (high < 0 || high >= 64 || low < 0 || low >= 64) {
                 PyErr_SetString(PyExc_ValueError, "Illegal character found.");
                 return -1;
             }
@@ -67,7 +63,7 @@ decode_intensity(const char *encoded, int length, int16_t *signals)
 static PyObject *
 Py_decode_intensity(PyObject *self, PyObject *args)
 {
-    PyObject *r;
+    PyArrayObject *r;
     char *encoded;
     Py_ssize_t encoded_length;
     npy_intp dim[2];
@@ -84,13 +80,13 @@ Py_decode_intensity(PyObject *self, PyObject *args)
     dim[0] = encoded_length / 8;
     dim[1] = NCHANNELS;
 
-    if ((r = PyArray_SimpleNew(2, dim, NPY_INT16)) == NULL)
+    if ((r = (PyArrayObject *)PyArray_SimpleNew(2, dim, NPY_INT16)) == NULL)
         return NULL;
 
     if (decode_intensity(encoded, dim[0], PyArray_DATA(r)) == -1)
         Py_XDECREF(r);
 
-    return r;
+    return (PyObject *)r;
 }
 
 static void
@@ -104,8 +100,8 @@ encode_intensity(int length, const int16_t *signals, char *outbuf)
             v = *(signals++) - SIGNAL_MIN;
             v = (v <= CODED_MAX ? v : CODED_MAX);
             v = (v >= 0 ? v : 0);
-            *(outbuf++) = base64_enc_table[v >> 6];
-            *(outbuf++) = base64_enc_table[v & 63];
+            *(outbuf++) = 33 + (v >> 6);
+            *(outbuf++) = 33 + (v & 63);
         }
     }
 }
@@ -113,10 +109,11 @@ encode_intensity(int length, const int16_t *signals, char *outbuf)
 static PyObject *
 Py_encode_intensity(PyObject *self, PyObject *args)
 {
-    PyObject *signals, *r;
+    PyArrayObject *signals;
+    PyObject *r;
     int length;
 
-    if (!PyArg_ParseTuple(args, "O:encode_intensity", &signals))
+    if (!PyArg_ParseTuple(args, "O:encode_intensity", (PyObject **)&signals))
         return NULL;
 
     if (!PyArray_Check(signals)) {
@@ -124,17 +121,18 @@ Py_encode_intensity(PyObject *self, PyObject *args)
         return NULL;
     }
 
-    if (PyArray_NDIM(signals) != 2 || PyArray_DIMS(signals)[1] != NCHANNELS ||
+    if (PyArray_NDIM(signals) != 2 ||
+            PyArray_DIMS(signals)[1] != NCHANNELS ||
             PyArray_DESCR(signals) != PyArray_DescrFromType(NPY_INT16)) {
         PyErr_Format(PyExc_TypeError, "array must be N x %d int16.", NCHANNELS);
         return NULL;
     }
 
     length = PyArray_DIMS(signals)[0];
-    if ((r = PyString_FromStringAndSize(NULL, length * 8)) == NULL)
+    if ((r = PyBytes_FromStringAndSize(NULL, length * 8)) == NULL)
         return NULL;
 
-    encode_intensity(length, PyArray_DATA(signals), PyString_AS_STRING(r));
+    encode_intensity(length, PyArray_DATA(signals), PyBytes_AS_STRING(r));
     return r;
 }
 
@@ -160,8 +158,8 @@ encode_intensity_from_string(const char *intxt, int ncycles, char *outbuf)
             v = intensity[chan] - SIGNAL_MIN;
             v = (v <= CODED_MAX ? v : CODED_MAX);
             v = (v >= 0 ? v : 0);
-            *(outbuf++) = base64_enc_table[v >> 6];
-            *(outbuf++) = base64_enc_table[v & 63];
+            *(outbuf++) = 33 + (v >> 6);
+            *(outbuf++) = 33 + (v & 63);
         }
     }
 
@@ -178,11 +176,11 @@ Py_encode_intensity_from_string(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "si:encode_intensity_from_string", &instr, &length))
         return NULL;
 
-    r = PyString_FromStringAndSize(NULL, length * NCHANNELS * 2);
+    r = PyBytes_FromStringAndSize(NULL, length * NCHANNELS * 2);
     if (r == NULL)
         return NULL;
 
-    outstr = PyString_AS_STRING(r);
+    outstr = PyBytes_AS_STRING(r);
     outstr[length * NCHANNELS * 2] = 0;
 
     if (encode_intensity_from_string(instr, length, outstr)) {
@@ -203,198 +201,17 @@ Py_phred_64to33(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s#:phred_64to33", &instr, &length))
         return NULL;
 
-    r = PyString_FromStringAndSize(NULL, length);
+    r = PyBytes_FromStringAndSize(NULL, length);
     if (r == NULL)
         return NULL;
 
-    outstr = PyString_AS_STRING(r);
+    outstr = PyBytes_AS_STRING(r);
     outstr[length] = 0;
 
     while (*instr != 0)
         *(outstr++) = *(instr++) - 31;
 
     return r;
-}
-
-
-/* ======================================================== */
-/* decode_index */
-
-#define MAX_INDICES         128
-#define MAX_INDEX_LENGTH    8
-
-typedef struct {
-    PyObject_HEAD
-    int indexlen;
-    int numindices;
-    char indices[MAX_INDICES][MAX_INDEX_LENGTH];
-} IndexDecoderObject;
-
-static PyTypeObject IndexDecoder_Type;
-
-#define IndexDecoder_Type(v)        (Py_TYPE(v) == &IndexDecoder_Type)
-
-static IndexDecoderObject *
-IndexDecoder_create(PyObject *args)
-{
-    IndexDecoderObject *self;
-    PyObject *indexlist=NULL;
-    Py_ssize_t i;
-
-    if (!PyArg_ParseTuple(args, "O:IndexDecoder", &indexlist))
-        return NULL;
-
-    if (!PyList_Check(indexlist)) {
-        PyErr_SetString(PyExc_TypeError, "List of index must be given in a list.");
-        return NULL;
-    }
-
-    self = PyObject_New(IndexDecoderObject, &IndexDecoder_Type);
-    if (self == NULL)
-        return NULL;
-
-    self->numindices = PyList_Size(indexlist);
-    self->indexlen = 0;
-
-    if (self->numindices > MAX_INDICES) {
-        PyErr_SetString(PyExc_TypeError, "too many indices.");
-        Py_DECREF(self);
-        return NULL;
-    }
-
-    for (i = 0; i < self->numindices; i++) {
-        PyObject *item;
-
-        item = PyList_GetItem(indexlist, i);
-        if (!PyString_Check(item)) {
-            PyErr_Format(PyExc_TypeError, "%d-th index is not a string.", (int)i + 1);
-            Py_DECREF(self);
-            return NULL;
-        }
-
-        if (i == 0) {
-            self->indexlen = PyString_Size(item);
-
-            if (self->indexlen > MAX_INDEX_LENGTH) {
-                PyErr_SetString(PyExc_TypeError, "index is too long.");
-                Py_DECREF(self);
-                return NULL;
-            }
-        }
-        else if (PyString_Size(item) != self->indexlen) {
-            PyErr_Format(PyExc_TypeError, "length of %d-th index is not %d.", (int)i + 1,
-                         (int)self->indexlen);
-            Py_DECREF(self);
-            return NULL;
-        }
-
-        memcpy(self->indices[i], PyString_AS_STRING(item), self->indexlen);
-    }
-
-    return self;
-}
-
-static void
-IndexDecoder_dealloc(IndexDecoderObject *self)
-{
-    PyObject_Del(self);
-}
-
-static PyObject *
-IndexDecoder_call(IndexDecoderObject *self, PyObject *args, PyObject *kw)
-{
-    char *inseq;
-    Py_ssize_t inseqlen;
-    int bestidx, bestmismatches, i, secondbestfound;
-
-    if (!PyArg_ParseTuple(args, "s#:decode", &inseq, &inseqlen))
-        return NULL;
-
-    if (inseqlen != self->indexlen) {
-        PyErr_SetString(PyExc_TypeError, "index size mismatch.");
-        return NULL;
-    }
-
-    bestidx = -1;
-    bestmismatches = self->indexlen + 1;
-    secondbestfound = 0;
-
-    for (i = 0; i < self->numindices; i++) {
-        int j, mismatches=0;
-
-        for (j = 0; j < self->indexlen; j++)
-            mismatches += (inseq[j] != self->indices[i][j]);
-
-        if (mismatches < bestmismatches) {
-            bestidx = i;
-            bestmismatches = mismatches;
-            secondbestfound = 0;
-        }
-        else if (mismatches == bestmismatches)
-            secondbestfound = 1;
-    }
-
-    if (bestidx == -1) {
-        PyErr_SetString(PyExc_ValueError, "no feasible index matched.");
-        return NULL;
-    }
-
-    if (secondbestfound == 1) {
-        PyErr_SetString(PyExc_ValueError, "multiple indices have same distance.");
-        return NULL;
-    }
-
-    return Py_BuildValue("(is#)", bestmismatches, self->indices[bestidx], self->indexlen);
-}
-
-static PyTypeObject IndexDecoder_Type = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    "tailseqext.IndexDecoder",           /*tp_name*/
-    sizeof(IndexDecoderObject),          /*tp_basicsize*/
-    0,                      /*tp_itemsize*/
-    /* methods */
-    (destructor)IndexDecoder_dealloc, /*tp_dealloc*/
-    0,                      /*tp_print*/
-    0,                      /*tp_getattr*/
-    0,                      /*tp_setattr*/
-    0,                      /*tp_compare*/
-    0,                      /*tp_repr*/
-    0,                      /*tp_as_number*/
-    0,                      /*tp_as_sequence*/
-    0,                      /*tp_as_mapping*/
-    0,                      /*tp_hash*/
-    (ternaryfunc)IndexDecoder_call, /*tp_call*/
-    0,                      /*tp_str*/
-    0,                      /*tp_getattro*/
-    0,                      /*tp_setattro*/
-    0,                      /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,     /*tp_flags*/
-    0,                      /*tp_doc*/
-    0,                      /*tp_traverse*/
-    0,                      /*tp_clear*/
-    0,                      /*tp_richcompare*/
-    0,                      /*tp_weaklistoffset*/
-    0,                      /*tp_iter*/
-    0,                      /*tp_iternext*/
-    0,                      /*tp_methods*/
-    0,                      /*tp_members*/
-    0,                      /*tp_getset*/
-    0,                      /*tp_base*/
-    0,                      /*tp_dict*/
-    0,                      /*tp_descr_get*/
-    0,                      /*tp_descr_set*/
-    0,                      /*tp_dictoffset*/
-    0,                      /*tp_init*/
-    0,                      /*tp_alloc*/
-    0,                      /*tp_new*/
-    0,                      /*tp_free*/
-    0,                      /*tp_is_gc*/
-};
-
-static PyObject *
-IndexDecoder_new(PyObject *self, PyObject *args)
-{
-    return (PyObject *)IndexDecoder_create(args);
 }
 
 
@@ -440,13 +257,13 @@ PolyALocator_create(PyObject *args)
         if (value == NULL)
             continue;
 
-        if (!PyInt_Check(value)) {
+        if (!PyLong_Check(value)) {
             PyErr_SetString(PyExc_TypeError, "Values of weight map must be an integer");
             Py_DECREF(self);
             return NULL;
         }
 
-        self->weight[(int)c] = (int)PyInt_AsLong(value);
+        self->weight[(int)c] = (int)PyLong_AsLong(value);
     }
 
     return self;
@@ -571,36 +388,62 @@ static PyMethodDef tailseqext_methods[] = {
         PyDoc_STR("encode_intensity_from_string(str) -> str")},
     {"phred_64to33", Py_phred_64to33, METH_VARARGS,
         PyDoc_STR("phred_64to33(str) -> str")},
-    {"IndexDecoder", IndexDecoder_new, METH_VARARGS,
-        PyDoc_STR("IndexDecoder(indices) -> object")},
     {"PolyALocator", PolyALocator_new, METH_VARARGS,
         PyDoc_STR("PolyALocator(weights) -> object")},
     {NULL, NULL}           /* sentinel */
 };
 
-PyDoc_STRVAR(module_doc,
+PyDoc_STRVAR(tailseqext_doc,
 "This is a template module just for instruction.");
 
-/* Initialization function for the module (*must* be called initxx) */
+struct tailseqext_state {
+    PyObject *error;
+};
 
-PyMODINIT_FUNC
-inittailseqext(void)
+#define GETSTATE(m) ((struct tailseqext_state*)PyModule_GetState(m))
+
+static int tailseqext_traverse(PyObject *m, visitproc visit, void *arg) {
+    Py_VISIT(GETSTATE(m)->error);
+    return 0;
+}
+
+static int tailseqext_clear(PyObject *m) {
+    Py_CLEAR(GETSTATE(m)->error);
+    return 0;
+}
+
+static struct PyModuleDef tailseqext_def = {
+    PyModuleDef_HEAD_INIT,
+    "tailseqext",
+    tailseqext_doc,
+    sizeof(struct tailseqext_state),
+    tailseqext_methods,
+    NULL,
+    tailseqext_traverse,
+    tailseqext_clear,
+    NULL
+};
+
+PyObject *
+PyInit_tailseqext(void)
 {
     PyObject *m;
 
     import_array();
 
     /* Create the module and add the functions */
-    m = Py_InitModule3("tailseqext", tailseqext_methods, module_doc);
+    m = PyModule_Create(&tailseqext_def);
     if (m == NULL)
-        return;
+        return NULL;
 
     /* Add some symbolic constants to the module */
-    if (ErrorObject == NULL) {
-        ErrorObject = PyErr_NewException("tailseqext.error", NULL, NULL);
-        if (ErrorObject == NULL)
-            return;
+    struct tailseqext_state *st = GETSTATE(m);
+
+    st->error = PyErr_NewException("tailseqext.Error", NULL, NULL);
+    if (st->error == NULL) {
+        Py_DECREF(m);
+        return NULL;
     }
-    Py_INCREF(ErrorObject);
-    PyModule_AddObject(m, "error", ErrorObject);
+
+    return m;
 }
