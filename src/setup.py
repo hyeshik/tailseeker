0dc09b3b08847e467681bcb3ada19bc48d093a5e
Hyeshik Chang <hyeshik@snu.ac.kr>
2016-04-08 22:58:44 +0900

    Remove sqi2fq.
    SQI is not used anymore.

--- src/setup.py
+++ src/setup.py
@@ -22,27 +22,12 @@
 #
 
 import os
-import subprocess as sp
-from distutils.core import setup, Extension
+from distutils.core import setup
 
 if 'TAILSEEKER_DIR' in os.environ:
     TAILSEEKER_DIR = os.environ['TAILSEEKER_DIR']
 else:
     TAILSEEKER_DIR = os.path.abspath('..')
-
-PKGCONFIG_CMD = os.environ.get('PKGCONFIG_CMD', 'pkg-config')
-
-# This function implementation was borrowed from George Snyder.
-# - http://code.activestate.com/recipes/502261-python-distutils-pkg-config/#c2
-def pkgconfig(package, **kw):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in sp.check_output("{} --libs --cflags {}".format(PKGCONFIG_CMD, package),
-                                 shell=True).decode('ascii').split():
-        if token[:2] in flag_map:
-            kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-        else:
-            kw.setdefault('extra_compile_args', []).append(token)
-    return kw
 
 def fix_tailseeker_path():
     content = open('tailseeker.in').read()
@@ -51,10 +36,6 @@ def fix_tailseeker_path():
 
 fix_tailseeker_path()
 
-srcfile = 'pythonext/tailseqext.c'
-modname = 'tailseqext'
-htslib_flags = pkgconfig('htslib')
-
 setup(name = modname,
       version = '0.2',
       description = 'Internal extension module for tailseeker',
@@ -62,6 +43,5 @@ setup(name = modname,
       author_email = 'hyeshik@snu.ac.kr',
       url = 'http://highthroughput.org',
       license = 'MIT',
-      ext_modules = [Extension(modname, [srcfile], **htslib_flags)],
       scripts = ['tailseeker']
 )
