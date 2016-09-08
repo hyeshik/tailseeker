d643539c449a951c9909c9a393f9f0474bb9b664
Hyeshik Chang <hyeshik@snu.ac.kr>
2016-09-09 01:35:09 +0900

    Fix broken path pointers to scratch/taginfo-fl/*.

--- tailseeker/level2_analysis.py
+++ tailseeker/level2_analysis.py
@@ -185,7 +185,9 @@ if CONF['performance']['enable_gsnap']:
             star='scratch/alignments/{sample}_STAR_{type}.bam',
             gsnap=expand('scratch/alignments/{{sample}}_GSNAP_{{type}}.bam.{part}',
                          part=range(CONF['performance']['split_gsnap_jobs'])),
-            taginfo=expand('scratch/taginfo-fl/{{sample}}_{tile}.txt.gz', tile=TILES)
+            taginfo=expand('scratch/taginfo-fl-r{round:02d}/{{sample}}_{tile}.txt.gz',
+                           tile=TILES,
+                           round=[CONF['polyA_ruler']['signal_resampling_rounds'] + 1])
         output: temp('scratch/merged-alignments/{sample}_{type,[^_.]+}.bam')
         threads: THREADS_MAXIMUM_CORE
         params: sorttmp='scratch/alignments/{sample}_merge_{type}'
@@ -199,7 +201,9 @@ else:
     rule merge_alignments:
         input:
             star='scratch/alignments/{sample}_STAR_{type}.bam',
-            taginfo=expand('scratch/taginfo-fl/{{sample}}_{tile}.txt.gz', tile=TILES)
+            taginfo=expand('scratch/taginfo-fl-r{round:02d}/{{sample}}_{tile}.txt.gz',
+                           tile=TILES,
+                           round=[CONF['polyA_ruler']['signal_resampling_rounds'] + 1])
         output: temp('scratch/merged-alignments/{sample}_{type,[^_.]+}.bam')
         threads: THREADS_MAXIMUM_CORE
         params: sorttmp='scratch/alignments/{sample}_merge_{type}'
